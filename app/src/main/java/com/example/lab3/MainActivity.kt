4cc144123884e86c24e9b50b14961687240d5251
Olga <Vasileva.o.p.2.0.0@gmail.com>
2024-10-09 23:04:23 +0700

    3 лабораторная работа Вариант 7. Дана строка. Заменить в ней указанный символ другим. Символы ввести с клавиатуры.

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.lab3
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val originalTextView = findViewById<TextView>(R.id.originalTextView)
+        val editTextText = findViewById<EditText>(R.id.editTextText)
+        val editTextText2 = findViewById<EditText>(R.id.editTextText2)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val charToReplace = editTextText.text.toString()
+            val newChar = editTextText2.text.toString()
+
+
         }
     }
 }
